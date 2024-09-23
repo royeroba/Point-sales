b34d49157dd1fbb23d5542b51da4d11efcab8d42
royerobadev <royeroba2007@gmail.com>
2024-09-23 13:45:40 -0500

    change title header

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,7 +1,6 @@
 <template>
-    <h1 class="text-3xl font-black text-white">
-        POS
-        <span class="text-green-400">Vue.js</span>
-    </h1>
+  <h1 class="text-3xl font-black text-white">
+    Point of
+    <span class="text-green-400">Sales</span>
+  </h1>
 </template>
-
